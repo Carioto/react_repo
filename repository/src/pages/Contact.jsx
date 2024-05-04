06c321a79977d5bb752623d6489f18a0dadd24c1
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-05-04 12:54:15 -0400

    add contact me content

--- repository/src/pages/Contact.jsx
+++ repository/src/pages/Contact.jsx
@@ -1,9 +1,75 @@
+import { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
+
+
+
 function Contact(){
+  const [fullName, setFullName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [comment, setComment] = useState('');
+  const [anyMessage, setAnyMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'emailAddress'){
+        setEmailAddress(value)
+    }else if (name === 'fullName'){
+        setFullName(value)
+    }else { 
+        setComment(value)
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(emailAddress) || !fullName){
+        setAnyMessage('Invalid name or email');
+        return;
+    }
+        console.log(fullName,emailAddress,comment)
+        setComment('');
+        setFullName('');
+        setEmailAddress('');
+        setAnyMessage('Thanks!');
+        return;
+  }
+
     return(
-      <div>
-
-
+      <div className='container text-center'>
+        <h1>Contact me via Email</h1>
+        <form className='form' onSubmit={handleFormSubmit}>
+          <input
+            value={fullName}
+            name='fullName'
+            onChange={handleInputChange}
+            type='text'
+            placeholder='Your Name'
+          />
+          <input
+            value={emailAddress}
+            name='emailAddress'
+            onChange={handleInputChange}
+            type='email'
+            placeholder='Email'
+          />
+          <input
+            value={comment}
+            name='comment'
+            onChange={handleInputChange}
+            type='text'
+            placeholder='Your Text'
+          />
+          <button type='submit'>
+            Submit
+          </button>
+        </form>
+        {anyMessage && (
+          <div>
+            <p className='any-text'>{anyMessage}</p>
+          </div>
+        )}
       </div>
-    )
+    );
 }
 export default Contact;
