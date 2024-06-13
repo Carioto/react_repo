8cbd998ba7b8936afa1b0749fe7a4a48ffbb1a04
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-06-13 13:20:50 -0400

    tweak mobile layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ function Navigation() {
     const currentPage = useLocation().pathname;
     return (
       <div id='navbar'>
-      <ul className='nav nav-tabs'>
+      <ul className='nav nav-tabs navdis'>
          <li className='nav-item'>
             <Link
              to = "/"
