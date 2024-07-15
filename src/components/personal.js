a9b8db227d77ccdf96118cc3a06d6114e16ca86e
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-07-14 21:07:57 -0400

    Add personal Projects section

---
+++ src/components/personal.js
@@ -0,0 +1,13 @@
+const personal = [
+    // set images to 300 x 180
+        {
+            id:8,
+            title:"MongoDB Sample Flix Database",
+            deployedUrl:"https://mongodb-movies.onrender.com/",
+            githubUrl:"https://github.com/Carioto/mongodb-movies",
+            image:"./mdb_mflix.jpg",
+    
+        }
+]
+
+export default personal;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,14 @@
 import Project from '../components/Project';
 import projects from '../components/projects'
+import personal from '../components/personal';
 
 function Portfolio(){
     return(
       <div>
-        <h2>Projects</h2>
+        <h2>Personal Projects</h2>
+        <Project projects={personal} />
+
+        <h2>Boot Camp Projects - Solo and Group</h2>
         <Project projects={projects} />
       </div>
     );
