21a92338aa488b1c3912900f28203a33ad15106f
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-07-21 15:54:20 -0400

    Add Bookmarks Holder

--- src/components/personal.js
+++ src/components/personal.js
@@ -1,13 +1,21 @@
 const personal = [
     // set images to 300 x 180
         {
+            id:7,
+            title:"Personal Bookmarks Holder",
+            deployedUrl:"https://bookmark-holder.onrender.com/",
+            githubUrl:"https://github.com/Carioto/bookmark_holder",
+            image:"./PBookmark.png",
+    
+        },
+        {
             id:8,
             title:"MongoDB Sample Flix Database",
             deployedUrl:"https://mongodb-movies.onrender.com/",
             githubUrl:"https://github.com/Carioto/mongodb-movies",
             image:"./mdb_mflix.jpg",
     
-        }
+        },
 ]
 
 export default personal;
