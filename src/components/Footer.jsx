2b1d89393b6a89baf537211693eff6834498697d
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-11-24 21:13:47 -0500

    change background, footer animation added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
 return (
     <div>
      <div id="foot">
-        <a className=".img-fluid" href="https://github.com/Carioto/">
+        <a  className=".img-fluid gitImg" href="https://github.com/Carioto/">
             <img src={"/GitHub.png"} alt="GitHub"/>
         </a>
       
-        <a className=".img-fluid" href="https://www.linkedin.com/in/david-carioto">
+        <a className=".img-fluid linkImg" href="https://www.linkedin.com/in/david-carioto">
             <img src={"/Linkedin.png"} alt="Linkedin"/>
         </a>
       
