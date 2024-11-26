42d36732a46899ad0ef4aa6d8407da09f9587d9b
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-11-26 10:25:08 -0500

    header animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,20 @@
 function Header() {
     return(
      <div>
-      <h1>David Carioto</h1>
+      <h1>
+        <span id='dmove'>D</span>
+        <span id='amove'>a</span>
+        <span id='vmove'>v</span>
+        <span id='imove'>i</span>
+        <span id='dmove'>d</span>&nbsp;
+        <span id='cmove'>C</span>
+        <span id='amove'>a</span>
+        <span id='rmove'>r</span>
+        <span id='imove'>i</span>
+        <span id='omove'>o</span>
+        <span id='tmove'>t</span>
+        <span id='omove'>o</span>
+        </h1>
       <p id='headP'>&nbsp;&nbsp; --Full Stack Web Developer in Pittsburgh, Pennsylvania</p>
      </div>
     )
